259646796e7e7606e5a8defd4622e6be4d9977b7
Abdulwaarith <abdulwaarithz@gmail.com>
2026-09-26 12:46:59 +0300

    Export StatusPage from @/layouts

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -2,3 +2,4 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export type { BannerTone } from "./PageBanner";
 export { default as PageBanner } from "./PageBanner";
+export { STATUS_PRIMARY_BUTTON, STATUS_SECONDARY_BUTTON, default as StatusPage } from "./StatusPage";
