131bfc4c2c75fe5ad6726b0db3578d82379bd009
Abdulwaarith <abdulwaarithz@gmail.com>
2026-09-26 12:46:56 +0300

    Add @/hooks barrel

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from "./Audio";
+export * from "./Library";
+export type { ShortcutMap } from "./useKeyboardShortcuts";
+export { default as useKeyboardShortcuts } from "./useKeyboardShortcuts";
